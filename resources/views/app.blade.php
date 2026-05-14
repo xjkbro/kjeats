c46ee57069ee14022bc1a65fb4a0873e90d45d95
Jason Kyle De Lara <jason8oh8@gmail.com>
2026-05-14 07:40:09 -0700

    Add new favicon images and site web manifest
    - Added apple-touch-icon.png for iOS devices.
    - Added favicon-16x16.png and favicon-32x32.png for browser compatibility.
    - Added favicon.ico for legacy support.
    - Created site.webmanifest for PWA support with specified icons and colors.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,9 +31,14 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        {{-- <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/favicon/site.webmanifest">
 
         @fonts
         <link rel="preconnect" href="https://fonts.googleapis.com">
