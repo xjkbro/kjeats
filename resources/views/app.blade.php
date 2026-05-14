f08b2a4cc677d6759408e34a23eb0294b1b187fa
Jason Kyle De Lara <jason8oh8@gmail.com>
2026-05-14 12:33:30 -0700

    fix: update viewport meta tag to include maximum scale for better responsiveness

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         @PwaHead
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
